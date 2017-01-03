eb7218611d17917f65da05d4a4d88a6ce2659d59
Arman Chatikyan <armcha01@gmail.com>
2017-01-03 20:06:49 +0400

    Added gif and sample apk

--- app/src/main/java/com/luseen/simplepermissions/MainActivity.java
+++ app/src/main/java/com/luseen/simplepermissions/MainActivity.java
@@ -1,26 +1,40 @@
 package com.luseen.simplepermissions;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.luseen.simplepermission.permissions.Permission;
 import com.luseen.simplepermission.permissions.PermissionActivity;
+import com.luseen.simplepermission.permissions.PermissionUtils;
 import com.luseen.simplepermission.permissions.SinglePermissionCallback;
 
 public class MainActivity extends PermissionActivity {
 
     public static final String TAG = "SimplePermission";
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        requestPermission(Permission.READ_SMS, new SinglePermissionCallback() {
+        textView = (TextView) findViewById(R.id.textView);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPermissionResult(boolean permissionGranted, boolean isPermissionDeniedForever) {
-                Log.d(TAG, "Permission granted = " + permissionGranted);
-                Log.d(TAG, "Permission denied forever = " + isPermissionDeniedForever);
+            public void onClick(View v) {
+                if (PermissionUtils.isMarshmallowOrHigher()) {
+                    requestPermission(Permission.READ_SMS, new SinglePermissionCallback() {
+                        @Override
+                        public void onPermissionResult(boolean permissionGranted, boolean isPermissionDeniedForever) {
+                            textView.setText("Permission granted = " + permissionGranted +
+                                    "\nPermission denied forever = " + isPermissionDeniedForever);
+
+                        }
+                    });
+                } else {
+                    textView.setText("PermissionUtils.isMarshmallowOrHigher() = false");
+                }
             }
         });
     }
